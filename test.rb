7ee5d123f56f87be4a844a525f8898e890dae849
aguglielmo <a@thruview.net>
2013-01-06 16:32:53 -0500

    Update test.rb

--- test.rb
+++ test.rb
@@ -3,7 +3,8 @@ require 'json'
 json = RestClient.get 'https://raw.github.com/aguglielmo/villian/master/sampledata'
 data = JSON.parse json
 if data['numbers'].grep(/^#{callerID}/)
-new_call = call "tel:+#{$callerID}", { :network => "SMS" }
+new_call = call "tel:+#{$callerID}", { :network => "SMS", :callerID=>"14809996171"
+ }
 new_call.say "You have requested service in the last thirty days, please install this app and our staff will be in touch in less than 5 minutes, thruview.net "
 new_call.hangup
 else
